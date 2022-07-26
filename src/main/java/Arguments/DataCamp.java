c1b60a1f60d2807bd1c24660412b762a892bb49f
Kauser Ahmed <kauserahmed@kausers-mbp.myfiosgateway.com>
2022-07-26 00:29:15 -0400

    late nightSnack

--- src/main/java/Arguments/DataCamp.java
+++ src/main/java/Arguments/DataCamp.java
@@ -3,8 +3,36 @@ package Arguments;
 public class DataCamp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Print 1 to 10 using while loop.
+		int x = 1;
+		while(x<=10)
+		{
+			System.out.println(x);
+			x++;
+		}
+				
 
 	}
 
 }
+//
+
+// We all have to finish below task before next class which will be held on Saturday 30th July
+/*
+* 01.Create a method with no arguments
+* 02.Create a method which return void;
+* 03.Create a method which return a boolean value;
+* 04.Create a method with one parameter Data type is integer 
+* 05.Create a method with Two parameter Data type is String 
+* 06.Create a method with Two parameter Data type is integer and String 
+* 07.Create a method with Three parameter Data type is integer, String , boolean;
+* 08.Create a method which accept two integer arguments and return sum of those two integer;
+* 09.Create a method which accept two string arguments and return boolean value;
+* 10.Print 1 to 10 using while loop.
+* 
+* 
+*/
+
+
+//Task for 1st August to 6th August  is coming soon.
